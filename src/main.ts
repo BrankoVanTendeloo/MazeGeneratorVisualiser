9081a325d622bac55b12496a9af31d9b1670a58d
Branko Van Tendeloo <brankovt1bvt@gmail.com>
2026-05-15 20:18:26 +0200

    Added radio button control

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { HomePage } from "./pages/home/home";
 import { MazeDrawer } from './models/maze_drawer';
 import { MazeGraph } from './models/maze_graph';
 
+type renderType = "walls" | "graph" | "both"
+
 const homePage = new HomePage()
 homePage.render()
 
@@ -13,12 +15,31 @@ const widthRange = document.querySelector<HTMLInputElement>("#widthRange")!
 const heightSpan = document.querySelector<HTMLSpanElement>("#height-current-value")!
 const widthSpan = document.querySelector<HTMLSpanElement>("#width-current-value")!
 const canvas = document.querySelector<HTMLCanvasElement>("#maze-canvas")!
+const radios = document.querySelectorAll<HTMLInputElement>("input[name='maze-display']")
 
 let maze: MazeGraph = new MazeGraph(+heightRange.value, +widthRange.value)
 let mazeDrawer: MazeDrawer = new MazeDrawer(maze, canvas, "both")
-
 let generating: boolean = false
 
+// radio buttons function
+for (let radio of radios) {
+    radio.addEventListener("change", (e) => {
+        if (radio.checked) {
+            if (radio.value === "walls") mazeDrawer.changeRenderType("walls")
+            else if (radio.value === "graph") mazeDrawer.changeRenderType("graph")
+            else mazeDrawer.changeRenderType("both")
+        }
+    })
+}
+
+function getRenderStyle() {
+    for (let radio of radios) if (radio.checked) {
+        if (radio.value === "walls") return "walls"
+        else if (radio.value === "graph") return "graph"
+        else return "both"
+    }
+    return "both"
+}
 
 // set canvas size
 const sizeOfCanvas = canvas.getBoundingClientRect()
@@ -59,5 +80,5 @@ widthRange.addEventListener("input", () => {
 // Reset maze
 function resetMaze(): void {
     maze = new MazeGraph(+heightRange.value, +widthRange.value)
-    mazeDrawer = new MazeDrawer(maze, canvas, "both")
+    mazeDrawer = new MazeDrawer(maze, canvas, getRenderStyle())
 }
